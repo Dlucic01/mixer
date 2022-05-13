effc7e506468519170ec98b9bb57c5d6089bba78
Dlucic01 <61463891+Dlucic01@users.noreply.github.com>
2022-05-13 17:03:29 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -7,8 +7,8 @@ error_reporting(E_ALL);
 
 session_start();
 
-define('DB_USER', 'exof');
-define('DB_PASS', 'Mumija12');
+define('DB_USER', '');
+define('DB_PASS', '');
 define('DBC', 'mysql:host=localhost;dbname=sound_vib;charset=UTF8');
 define('GUSER', 'franjotudman16@gmail.com');
 define('GPASS', 'Anubismon123');
